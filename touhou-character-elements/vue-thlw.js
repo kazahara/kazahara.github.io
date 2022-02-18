897aa8d193e0baf352892e6a8641d795ee6e79b3
Kazahara Ayumu <kazahara.ayumu+dev@gmail.com>
2022-02-18 07:32:57 +0300

    v1.2

--- touhou-character-elements/vue-thlw.js
+++ touhou-character-elements/vue-thlw.js
@@ -1,15 +1,12 @@
-function separate_elements(text){
-  if(!text) return ""
-
-  let r = /\d \w+/g
-  let a = [...text.match(r)]
-  return a
-}
-
 Vue.createApp({
   data() {
     return {
       characters: [],
+      owned_characters: [],
+      display_all_characters: false,
+      show_only_owned_characters: false,
+      display_names_only: false,
+      isModalVisible: false,
       EA: false,
       FI: false,
       ME: false,
@@ -18,110 +15,74 @@ Vue.createApp({
       SU: false,
       WA: false,
       WO: false,
-      name: "",
-      spread_target: "Solo",
-      spread_p0: "",
-      spread_p1: "",
-      spread_p2: "",
-      spread_p3: "",
-      focus_target: "Solo",
-      focus_p0: "",
-      focus_p1: "",
-      focus_p2: "",
-      focus_p3: "",
-      SC1_target: "Solo",
-      SC1_p0: "",
-      SC1_p1: "",
-      SC1_p2: "",
-      SC1_p3: "",
-      SC2_target: "Solo",
-      SC2_p0: "",
-      SC2_p1: "",
-      SC2_p2: "",
-      SC2_p3: "",
-      LW_target: "Solo",
-      LW_p0: "",
-      LW_p1: "",
-      LW_p2: "",
-      LW_p3: "",
+    }
+  },
+  computed: {
+    filtered_characters(){
+      if(this.display_all_characters) return this.sorted_characters
+      else return this.filtered_owned_characters
+    },
+    filtered_owned_characters() {
+      return this.sorted_characters.filter(character => character.owned)
+    },
+    sorted_characters(){
+      return this.characters.sort((a, b) => {
+        if (a.name < b.name) {
+          return -1;
+        }
+        if (a.name > b.name) {
+          return 1;
+        }
+        return 0;
+      })
     }
   },
   methods: {
-    add_character() {
-      this.characters.push({
-        "name": this.name,
-        "attacks": [
-          {
-            "target": this.spread_target,
-            "boost": [this.spread_p0, separate_elements(this.spread_p1), separate_elements(this.spread_p2), separate_elements(this.spread_p3)]
-          },
-          {
-            "target": this.focus_target,
-            "boost": [this.focus_p0, separate_elements(this.focus_p1), separate_elements(this.focus_p2), separate_elements(this.focus_p3)]
-          },
-          {
-            "target": this.SC1_target,
-            "boost": [this.SC1_p0, separate_elements(this.SC1_p1), separate_elements(this.SC1_p2), separate_elements(this.SC1_p3)]
-          },
-          {
-            "target": this.SC2_target,
-            "boost": [this.SC2_p0, separate_elements(this.SC2_p1), separate_elements(this.SC2_p2), separate_elements(this.SC2_p3)]
-          },
-          {
-            "target": this.LW_target,
-            "boost": [this.LW_p0, separate_elements(this.LW_p1), separate_elements(this.LW_p2), separate_elements(this.LW_p3)]
-          }
-        ],
-      })
-      this.reset()
-    },
     highlight(raw_elem) {
       let element = raw_elem.substring(2,4)
       if(this[element]) return element
     },
-    toggleWeakElement(element) {
+    toggle_weak_element(element) {
       this[element] = !this[element]
     },
-    reset(){
-      this.name = ""
-      this.spread_target = "Solo"
-      this.spread_p0 = ""
-      this.spread_p1 = ""
-      this.spread_p2 = ""
-      this.spread_p3 = ""
-      this.focus_target = "Solo"
-      this.focus_p0 = ""
-      this.focus_p1 = ""
-      this.focus_p2 = ""
-      this.focus_p3 = ""
-      this.SC1_target = "Solo"
-      this.SC1_p0 = ""
-      this.SC1_p1 = ""
-      this.SC1_p2 = ""
-      this.SC1_p3 = ""
-      this.SC2_target = "Solo"
-      this.SC2_p0 = ""
-      this.SC2_p1 = ""
-      this.SC2_p2 = ""
-      this.SC2_p3 = ""
-      this.LW_target = "Solo"
-      this.LW_p0 = ""
-      this.LW_p1 = ""
-      this.LW_p2 = ""
-      this.LW_p3 = ""
+    toggle_owned_characters(name, index){
+      this.characters[index].owned = !this.characters[index].owned
+      if(this.owned_characters.includes(name)) this.owned_characters.splice(this.owned_characters.indexOf(name), 1)
+      else this.owned_characters.push(name)
+      
+      this.save_owned_characters()
     },
-    save(){
-      let a = document.createElement("a")
-      let file = new Blob([JSON.stringify(this.characters)], {type: "text/plain"})
-      a.href = URL.createObjectURL(file)
-      a.download = "character_elements.json"
-      a.click()
+    save_owned_characters(){
+      const parsed = JSON.stringify(this.owned_characters)
+      localStorage.setItem("owned_characters", parsed)
+    },
+    show_modal() {
+      this.isModalVisible = true;
+    },
+    close_modal() {
+      this.isModalVisible = false;
     }
   },
-  created() {
-    fetch("https://kazahara.github.io/touhou-character-elements/character_elements.json")
+  mounted() {
+    // fetch("https://kazahara.github.io/touhou-character-elements/character_elements.json")
+    fetch("http://localhost:8000/character_elements.json")
       .then(response => response.json())
       .then(characters => {
+        if(localStorage.getItem("owned_characters")){
+          try {
+            this.owned_characters = JSON.parse(localStorage.getItem("owned_characters"))
+          } catch (e) {
+            localStorage.removeItem("owned_characters")
+          }
+        }
+
+        characters.forEach(character => {
+          let isOwned = false
+          if(this.owned_characters.includes(character.name)) isOwned = true
+
+          character.owned = isOwned
+        })
+
         this.characters = characters
       })
   }
